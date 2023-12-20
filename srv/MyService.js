0cd5ade53e65ea18709a4e55de98c1f76d14d58f
vykuntarao tangudu <vykuntoasis@gmail.com>
2023-12-20 11:22:09 +0000

    crud

--- srv/MyService.js
+++ srv/MyService.js
@@ -29,13 +29,67 @@ const mysrvdemo = function(srv){
         }else{
             results = await cds.tx(req).run(SELECT.from(employees).limit(1));
         }
-
-          
-
-
+  
         return results;
 
 
+    });
+    
+    srv.on("CREATE", "InserEmployeeSrv", async(req,res) => {
+        var dataSet = req.data;
+        let returnData = await cds.transaction(req).run([
+            INSERT.into(employees).entries(dataSet)
+        ])
+        .then((resolve, reject)=>{
+            if(typeof(resolve) !== undefined){
+                return req.data;
+            }else{
+                req.error(500,"Create Failed");
+            }
+        })
+        .catch( err => {
+            req.error(500,"There was an error "+ err.toString());
+        });
+        return returnData;
+    });
+
+    srv.on("UPDATE", "UpdateEmployeeSrv", async(req,res) => {
+        var dataSet = req.data;
+        let returnData = await cds.transaction(req).run([
+            UPDATE(employees).set({
+                nameFirst: req.data.nameFirst
+            }).where({ID: req.data.ID}),
+            UPDATE(employees).set({
+                nameLast: req.data.nameLast
+            }).where({ID: req.data.ID})
+        ]).then((resolve, reject)=>{
+            if(typeof(resolve) !== undefined){
+                return req.data;
+            }else{
+                req.error(500,"Create Failed");
+            }
+        }).catch( err => {
+            req.error(500,"There was an error "+ err.toString());
+        });
+        return returnData;
+    });
+
+    srv.on("DELETE", "DeleteEmployeeSrv", async(req,res) => {
+        var dataSet = req.data;
+        console.log(req.data.ID)
+        let returnData = await cds.transaction(req).run([
+            DELETE.from(employees).where({ID: req.data.ID})
+        ]).then((resolve, reject)=>{
+            if(typeof(resolve) !== undefined){
+                return req.data;
+            }else{
+                req.error(500,"Create Failed");
+            }
+        }).catch( err => {
+            req.error(500,"There was an error "+ err.toString());
+        });
+
+        return returnData;
     });
 
 };
